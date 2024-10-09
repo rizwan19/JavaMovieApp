40719401b5f63bee30859eb069a9d2d5a1d3c282
Rizwan Khan <rizwanulhaque19@gmail.com>
2024-10-09 14:31:24 +0600

    added current user

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -7,9 +7,11 @@ import java.util.Map;
 
 public class UserService {
     Map <String, User> userMap = new HashMap<>();
+    User currentUser;
     public User register(User newUser) {
         if(!userMap.containsKey(newUser.getEmail())){
             userMap.put(newUser.getEmail(), newUser);
+            currentUser = newUser;
             return newUser;
         }
         return null;
